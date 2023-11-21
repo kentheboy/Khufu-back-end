779c0f4409f7f3fe38ed125bebd88762c4517fab
kentheboy <foryoukentheboy>
2023-11-22 01:00:59 +0900

    update deploy.sh #5

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,26 @@
 php artisan down
-echo "Deploy inprogress...\n"
+echo "Deploy inprogress..."
+
+echo "git checkout ."
 git checkout .
+
+echo "git pull"
 git pull
+
+echo "composer install --no-dev --optimize-autoloader"
 composer install --no-dev --optimize-autoloader
+
+echo "php artisan migrate --force"
 php artisan migrate --force
+
+echo "php artisan config:cache"
 php artisan config:cache
+
+echo "php artisan view:clear"
 php artisan view:clear
+
+echo "php artisan cache:clear"
 php artisan cache:clear
+
 php artisan up
 echo "Deploy work completed!\n"
