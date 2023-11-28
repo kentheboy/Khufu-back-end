19e246e4ba0cc674fafe4a221fc067d7e8e27901
kentheboy <foryoukentheboy>
2023-11-28 21:14:12 +0900

    enable Product saving

--- app/Http/Controllers/Khufu/ProductsController.php
+++ app/Http/Controllers/Khufu/ProductsController.php
@@ -5,14 +5,27 @@ namespace App\Http\Controllers\Khufu;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Khufu\ProductCreateRequest;
+use App\Models\Khufu\Product;
 
 class ProductsController extends Controller
 {
     public function create(ProductCreateRequest $request){
 
+        $name = $request->name;
+        $description = $request->description;
+        $price = $request->price;
+        $customfields = $request->customfields;
+
+        Product::create([
+            'name' => $name,
+            'description' => $description,
+            'price' => $price,
+            'status' => 1,
+            'custom_field' => $customfields,
+        ]);
+
         $response = [
-            'msg' => "test api request successfully made",
-            'requestContent' => $request
+            'msg' => "test api request successfully made"
         ];
 
         return response($response);
